03aec639acf03e67a95eb25ff91b6a43566b59f0
Marc-Etienne <mecharron@code201.ca>
2020-11-18 14:55:00 -0500

    env to secret_key

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from secrets import *
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
