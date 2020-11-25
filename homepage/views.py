e11ffaa4b8918b3b26bad6001ea9fd2bcd3e5f3e
Marc-Etienne <mecharron@code201.ca>
2020-11-25 17:41:06 -0500

    add title variable

--- homepage/views.py
+++ homepage/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'homepage/index.html', {})
+    context = {'title': 'Django'}
+    return render(request, 'homepage/index.html', context)
